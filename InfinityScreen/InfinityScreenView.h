349e8972163c15aa530747834c6b0916b37e3c9f
MonsterSucker <martinvegance@live.com>
2016-10-10 20:07:54 +0800

    initials

---
+++ InfinityScreen/InfinityScreenView.h
@@ -0,0 +1,13 @@
+//
+//  InfinityScreenView.h
+//  InfinityScreen
+//
+//  Created by MonsterSucker on 10/10/2016.
+//  Copyright © 2016 MonsterTechStudio. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface InfinityScreenView : ScreenSaverView
+
+@end
---
+++ InfinityScreen/InfinityScreenView.m
@@ -0,0 +1,52 @@
+//
+//  InfinityScreenView.m
+//  InfinityScreen
+//
+//  Created by MonsterSucker on 10/10/2016.
+//  Copyright © 2016 MonsterTechStudio. All rights reserved.
+//
+
+#import "InfinityScreenView.h"
+
+@implementation InfinityScreenView
+
+- (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
